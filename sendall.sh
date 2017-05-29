504d88d3a4a48ccf5621bc28d3b1cb698a196dc1
irjdekker <ir.j.dekker@gmail.com>
2017-05-29 08:52:09 +0200

    Commit

--- sendall.sh
+++ sendall.sh
@@ -2,13 +2,18 @@
 #!/bin/bash
 workdirectory=`dirname "$(readlink -f "$0")"`
 
+checkfile="$workdirectory/start"
 filename="$workdirectory/maillist.txt"
+   
+if [ -f $checkfile ]; then
+  echo Starting mailing...
 
-echo Starting mailing...
+  while read p; do 
+    echo $p | awk -F"," '{system("./mail.sh \""$1 "\" \"" $2 "\"")}'
+    sleep 30
+  done < $filename
 
-while read p; do 
-  echo $p | awk -F"," '{system("./mail.sh \""$1 "\" \"" $2 "\"")}'
-  sleep 30
-done < $filename
-
-echo End mailing!
+  echo End mailing!
+  
+  rm -f $checkfile
+fi
