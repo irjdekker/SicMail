b2f23042ea9362eca80675dd0371b4d5e1cf6678
Fleur Kappen <ec2-user@mail.itlity.nl>
2017-06-04 21:09:21 +0200

    Commit

--- checkmail.sh
+++ checkmail.sh
@@ -17,6 +17,6 @@ echo "<head>"
 echo "<title>Pull Update</title>"
 echo "</head>"
 echo "<body>"
-cat $workdirectory/check_output.txt | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/<br>\n/g'
+cat $workdirectory/check_output.txt | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/<br>\n/g' | sed -e 's/,/, /g'
 echo "</body>"
 ) | /usr/sbin/sendmail -t
