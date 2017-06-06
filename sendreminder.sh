273951b78c0f9ba252f351b4d5e4df4b67c9cd92
Fleur Kappen <ec2-user@mail.itlity.nl>
2017-06-06 18:52:10 +0200

    Commit

--- sendreminder.sh
+++ sendreminder.sh
@@ -6,15 +6,16 @@ checkfile="$workdirectory/reminder/start.txt"
 filename="$workdirectory/reminder/list.txt"
 
 if [ -f $checkfile ]; then
-  rm -f $checkfile
+  #rm -f $checkfile
   /usr/bin/git commit -a -m "Commit"
   /usr/bin/git push
 
   echo Starting mailing... > $workdirectory/sendreminder.log
 
   while read p; do
-    echo $p | awk -F"," '{system("$./reminder/mail.sh \""$1 "\" \"" $2 "\"")}' >> $workdirectory/sendreminder.log
-    sleep 30
+    #echo $p | awk -F"," '{system("$./reminder/mail.sh \""$1 "\" \"" $2 "\"")}' >> $workdirectory/sendreminder.log
+    echo $p | awk -F"," '{system(echo \""$1 "\" \"" $2 "\"")}'
+    #sleep 30
   done < $filename
 
   echo End mailing! >> $workdirectory/sendreminder.log
